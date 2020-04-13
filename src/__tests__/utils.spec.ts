467a3985283c6ba97ee4204f65d9e362dde629ce
Fabian Schneider <fabbbbbi+git@googlemail.com>
2020-04-13 10:34:17 +0200

    :sparkles: Add utils

---
+++ src/__tests__/utils.spec.ts
@@ -0,0 +1,83 @@
+import R from 'ramda';
+import { overIf, overPaths, eachPath, eachOperation } from '../utils';
+
+describe('overIf', () => {
+  it('invokes over fn if key exists', () => {
+    const spyFn = jest.fn();
+    const fn = overIf(R.lensProp('test'), spyFn);
+    fn({ test: 'hello' });
+    expect(spyFn).toHaveBeenCalledWith('hello');
+  });
+
+  it('does not invoke over fn if key does not exists', () => {
+    const spyFn = jest.fn();
+    const fn = overIf(R.lensProp('test'), spyFn);
+    fn({ not: 'hello' });
+    expect(spyFn).not.toHaveBeenCalled();
+  });
+});
+
+describe('overPaths', () => {
+  const spyFn = jest.fn();
+  const fn = overPaths(spyFn);
+  const testPaths = {
+    '/hello': {
+      get: {},
+      post: {},
+    },
+  };
+
+  beforeEach(() => spyFn.mockReset());
+
+  it('invokes over fn on paths', () => {
+    fn({ paths: testPaths });
+    expect(spyFn).toHaveBeenCalledWith(testPaths);
+  });
+});
+
+describe('eachPath', () => {
+  const spyFn = jest.fn();
+  const fn = eachPath(spyFn);
+  const testPaths = {
+    '/hello': {
+      get: {},
+      post: {},
+    },
+    '/test': {
+      get: {},
+    },
+  };
+
+  it('invokes fn on each path', () => {
+    fn({ paths: testPaths });
+    expect(spyFn).toHaveBeenCalledTimes(2);
+    expect(spyFn).toHaveBeenNthCalledWith(1, testPaths['/hello']);
+    expect(spyFn).toHaveBeenNthCalledWith(2, testPaths['/test']);
+  });
+});
+
+describe('eachOperation', () => {
+  const spyFn = jest.fn();
+  const fn = eachOperation(spyFn);
+  const testPaths = {
+    '/hello': {
+      get: {},
+      post: {},
+    },
+    '/test': {
+      put: {},
+    },
+    '/v2/test': {
+      put: {},
+    },
+  };
+
+  it('invokes fn on each operation of each path', () => {
+    fn({ paths: testPaths });
+    expect(spyFn).toHaveBeenCalledTimes(4);
+    expect(spyFn).toHaveBeenNthCalledWith(1, testPaths['/hello']['get']);
+    expect(spyFn).toHaveBeenNthCalledWith(2, testPaths['/hello']['post']);
+    expect(spyFn).toHaveBeenNthCalledWith(3, testPaths['/test']['put']);
+    expect(spyFn).toHaveBeenNthCalledWith(4, testPaths['/v2/test']['put']);
+  });
+});
---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+import R from 'ramda';
+
+export const overIf = R.curry<
+  (lens: R.Lens, fn: R.Arity1Fn) => <T>(value: T) => T
+>((lens, fn) =>
+  R.unless<any, any>(
+    R.compose(
+      R.isNil,
+      R.view(lens)
+    ),
+    R.over(lens, fn)
+  )
+);
+
+export const overPaths = (fn: R.Arity1Fn) => overIf(R.lensProp('paths'), fn);
+
+export const eachPath = (fn: R.Arity1Fn) => overPaths(R.map(fn));
+
+export const eachOperation = (fn: R.Arity1Fn) => eachPath(R.map(fn));
